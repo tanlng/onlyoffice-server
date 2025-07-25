a8beb588edb62d6b4c624aa96e825d838680af63
Sergey Konovalov <Sergey.Konovalov@onlyoffice.com>
2025-07-25 17:42:45 +0300

    [feature] Update ai plugin dir

--- branding/info/ai/scripts/engine/providers/base.js
+++ branding/info/ai/scripts/engine/providers/base.js
@@ -1,3 +1,35 @@
+/*
+ * (c) Copyright Ascensio System SIA 2010-2025
+ *
+ * This program is a free software product. You can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License (AGPL)
+ * version 3 as published by the Free Software Foundation. In accordance with
+ * Section 7(a) of the GNU AGPL its Section 15 shall be amended to the effect
+ * that Ascensio System SIA expressly excludes the warranty of non-infringement
+ * of any third-party rights.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For
+ * details, see the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
+ *
+ * You can contact Ascensio System SIA at 20A-6 Ernesta Birznieka-Upish
+ * street, Riga, Latvia, EU, LV-1050.
+ *
+ * The  interactive user interfaces in modified source and object code versions
+ * of the Program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU AGPL version 3.
+ *
+ * Pursuant to Section 7(b) of the License you must retain the original Product
+ * logo when distributing the program. Pursuant to Section 7(e) we decline to
+ * grant you any rights under trademark law for use of our trademarks.
+ *
+ * All the Product's GUI elements, including illustrations and icon sets, as
+ * well as technical writing content are licensed under the terms of the
+ * Creative Commons Attribution-ShareAlike 4.0 International. See the License
+ * terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+ *
+ */
+
 "use strict";
 
 (function(){
@@ -154,6 +186,7 @@
 					if (provider.isOnlyDesktop() && (-1 === navigator.userAgent.indexOf("AscDesktopEditor")))
 						continue;
 
+					AI.providersWeights[provider.name] = provider.weight || i;
 					window.AI.InternalProviders.push(provider);
 				}
 			}
@@ -165,6 +198,7 @@
 
 	AI.InternalCustomProvidersSources = {};
 	AI.InternalCustomProviders = [];
+	AI.providersWeights = {};
 
 	AI.loadCustomProviders = function() {
 
@@ -218,6 +252,8 @@
 		if (AI.InternalCustomProvidersSources[name])
 			delete AI.InternalCustomProvidersSources[name];
 
+		let isChanged = false;
+
 		for (let i = 0, len = AI.InternalCustomProviders.length; i < len; i++) {
 			if (AI.InternalCustomProviders[i].name === name) {
 				AI.InternalCustomProviders.splice(i, 1);
@@ -226,10 +262,41 @@
 					delete AI.Providers[name];
 				}
 
-				AI.Storage.save();
-				AI.Storage.load();
+				isChanged = true;
 				break;
 			}				
+		}
+
+		let provider = AI.Providers[name];
+		if (!provider && name.endsWith("*")) {
+			name = name.substring(0, name.length - 1);
+			provider = AI.Providers[name];
+		}
+
+		if (provider) {
+			let internalProvider = null;
+			for (let j = 0, lenCustom = AI.InternalProviders.length; j < lenCustom; j++) {
+				if (AI.InternalProviders[j].name === name) {
+					internalProvider = AI.InternalProviders[j];
+					break;
+				}
+			}
+
+			if (!internalProvider) {
+				delete AI.Providers[i];
+				isChanged = true;
+			}
+
+			if (internalProvider.url !== provider.url || internalProvider.addon !== provider.addon) {
+				isChanged = true;
+				provider.url = internalProvider.url;
+				provider.addon = internalProvider.addon;
+			}
+		}
+
+		if (isChanged) {
+			AI.Storage.save();
+			AI.Storage.load();
 		}
 
 	};
@@ -240,8 +307,29 @@
 		for (let i = 0, len = AI.InternalCustomProviders.length; i < len; i++) {
 			names.push(AI.InternalCustomProviders[i].name);
 		}
+
+		for (let i in AI.Providers) {
+			let provider = AI.Providers[i];
+			if (!provider || !provider.name)
+				continue;
+
+			let internalProvider = null;
+			for (let j = 0, lenCustom = AI.InternalProviders.length; j < lenCustom; j++) {
+				if (AI.InternalProviders[j].name === provider.name) {
+					internalProvider = AI.InternalProviders[j];
+					break;
+				}
+			}
+
+			if (internalProvider && (internalProvider.url !== provider.url || internalProvider.addon !== provider.addon)) {
+				names.push(provider.name + "*");
+			}	
+		}
+
 		return names;
 
 	};
+
+	AI.serverSettings = null;
 
 })();
