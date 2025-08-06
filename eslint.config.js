c0b7849a0f8d5840beb8bc0c334b4305f0681585
Sergey Konovalov <Sergey.Konovalov@onlyoffice.com>
2025-08-06 18:55:44 +0300

    [git actions] Add GitHub Actions job for lint and format checks

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 const js = require('@eslint/js');
 const globals = require('globals');
 const prettier = require('eslint-config-prettier');
-const { includeIgnoreFile } = require('@eslint/compat');
+const {includeIgnoreFile} = require('@eslint/compat');
 const path = require('node:path');
 
 const gitignorePath = path.resolve(__dirname, '.gitignore');
@@ -15,8 +15,8 @@ module.exports = [
       ecmaVersion: 2022,
       sourceType: 'module',
       globals: {
-        ...globals.node,     // All Node.js globals automatically
-        ...globals.es2022    // Modern JavaScript globals
+        ...globals.node, // All Node.js globals automatically
+        ...globals.es2022 // Modern JavaScript globals
       }
     },
     rules: {
@@ -30,22 +30,22 @@ module.exports = [
         }
       ],
       'no-undef': 'error',
-      'eqeqeq': ['error', 'smart'],
-      'curly': ['error', 'all'],
-      
+      eqeqeq: ['error', 'smart'],
+      curly: ['error', 'all'],
+
       // Server-friendly settings
-      'no-console': 'off',                    // Allow console in server code
-      'no-continue': 'off',                   // Allow continue statements
-      'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-      
+      'no-console': 'off', // Allow console in server code
+      'no-continue': 'off', // Allow continue statements
+      'no-plusplus': ['error', {allowForLoopAfterthoughts: true}],
+
       // Modern JavaScript practices
-      'prefer-const': 'error',                // Enforce const for immutable bindings
-      'no-var': 'error',                      // Enforce let/const over var
-      'object-shorthand': 'error',            // Modern object property syntax
-      'prefer-arrow-callback': 'error',       // Modern callback style
-      'no-duplicate-imports': 'error',        // Avoid duplicate imports
-      'no-useless-constructor': 'error',      // Remove unnecessary constructors
-      'no-useless-return': 'error',           // Remove unnecessary return statements
+      'prefer-const': 'error', // Enforce const for immutable bindings
+      'no-var': 'error', // Enforce let/const over var
+      'object-shorthand': 'error', // Modern object property syntax
+      'prefer-arrow-callback': 'error', // Modern callback style
+      'no-duplicate-imports': 'error', // Avoid duplicate imports
+      'no-useless-constructor': 'error', // Remove unnecessary constructors
+      'no-useless-return': 'error', // Remove unnecessary return statements
       'max-lines': ['warn', 300]
     },
     ignores: [
