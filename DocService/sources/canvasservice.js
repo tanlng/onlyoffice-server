c8b7eafebc54b71009a8e62ce261a486bf2a6ad1
Sergey Konovalov <Sergey.Konovalov@onlyoffice.com>
2025-09-23 02:27:17 +0300

    Revert "[bug] Move from jimp to sharp; Fix bug 76854, 76727, 48506"
    This reverts commit 28c44687a5b61ca6050834f043b507fb41a149ae.

--- DocService/sources/canvasservice.js
+++ DocService/sources/canvasservice.js
@@ -770,10 +770,9 @@ function* commandImgurls(ctx, conn, cmd, outputData) {
 
       let outputUrl = {url: 'error', path: 'error'};
       if (data) {
-        // process image: fix EXIF rotation and convert unsupported formats to optimal format
-        data = yield utilsDocService.processImageOptimal(ctx, data);
+        data = yield utilsDocService.fixImageExifRotation(ctx, data);
 
-        const format = formatChecker.getImageFormat(ctx, data);
+        let format = formatChecker.getImageFormat(ctx, data);
         let formatStr;
         let isAllow = false;
         if (constants.AVS_OFFICESTUDIO_FILE_UNKNOWN !== format) {
@@ -793,6 +792,11 @@ function* commandImgurls(ctx, conn, cmd, outputData) {
           }
         }
         if (isAllow) {
+          if (format === constants.AVS_OFFICESTUDIO_FILE_IMAGE_TIFF) {
+            data = yield utilsDocService.convertImageToPng(ctx, data);
+            format = constants.AVS_OFFICESTUDIO_FILE_IMAGE_PNG;
+            formatStr = formatChecker.getStringFromFormat(format);
+          }
           let strLocalPath = 'media/' + crypto.randomBytes(16).toString('hex') + '_';
           if (urlParsed) {
             const urlBasename = pathModule.basename(urlParsed.pathname);
